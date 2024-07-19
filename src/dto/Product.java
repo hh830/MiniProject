f75c3d4af0e8966fdc679fe180cf641498457260
hh830 <jhh8890@naver.com>
2024-07-19 16:25:05 +0900

    상품조회 dto

---
+++ src/dto/Product.java
@@ -0,0 +1,106 @@
+package dto;
+
+public class Product {
+    private int productId;
+    private String pdname;
+    private int price;
+    private String content;
+    private String image;
+    private String brandName;
+    private int likeCount, brandId, categoryId;
+    private boolean userLiked;
+    private String has;
+
+    // getters and setters
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getPdname() {
+        return pdname;
+    }
+
+    public void setPdname(String pdname) {
+        this.pdname = pdname;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    public boolean getUserLiked() {
+        return userLiked;
+    }
+
+    public void setUserLiked(boolean userLiked) {
+        this.userLiked = userLiked;
+    }
+
+    public boolean isUserLiked() {
+        return userLiked;
+    }
+
+    public String getHas() {
+        return has;
+    }
+
+    public void setHas(String has) {
+        this.has = has;
+    }
+
+    public int getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(int brandId) {
+        this.brandId = brandId;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+}
